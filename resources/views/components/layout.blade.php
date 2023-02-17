cffacc1125ea96d24bcbf8ea2ce8eafd1389e0db
FisnikR <fisnikk97@gmail.com>
2023-02-17 18:47:38 +0100

    Learning Blade and Layouts
    Learning Blade  example.blade.php

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,12 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Blog</title>
+<link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+    {{ $slot }}
+</body>
+</html>

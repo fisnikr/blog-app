32429be31dfce925591bc891332fbb615cc79cf3
FisnikR <fisnikk97@gmail.com>
2023-03-01 19:26:21 +0100

    adding style to blog v1.0

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,5 +1,23 @@
 <x-layout>
-<div class="flex flex-col items-center p-12 mx-auto space-y-4">
+
+    <x-post-header />
+
+        <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+        <x-post-hero-card :post="$posts[0]" />
+
+            <div class="lg:grid lg:grid-cols-2">
+            <x-post-card />
+            <x-post-card />
+            </div>
+
+            <div class="lg:grid lg:grid-cols-3">
+               <x-post-card />
+               <x-post-card />
+               <x-post-card />
+            </div>
+        </main>
+
+<!-- <div class="flex flex-col items-center p-12 mx-auto space-y-4">
     @foreach ($posts as $post)
      <article class="bg-gray-100 p-4 space-y-2 rounded-md shadow-md">
 
@@ -18,5 +36,5 @@
         </div>
      </article>   
      @endforeach
-    </div>
+    </div> -->
 </x-layout>

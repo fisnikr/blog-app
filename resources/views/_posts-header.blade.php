fe7a21abdc482cea2d7bc080b260e0535d724fd8
FisnikR <fisnikk97@gmail.com>
2023-03-03 20:08:19 +0100

    Editing Styling and quick clean up

--- resources/views/_posts-header.blade.php
+++ resources/views/_posts-header.blade.php
@@ -2,15 +2,11 @@
 
 <header class="max-w-xl mx-auto mt-20 text-center">
     <h1 class="text-4xl">
-        Latest <span class="text-blue-500">Laravel From Scratch</span> News
+        NXK <span class="text-blue-500">Blog App</span> News
     </h1>
 
-    <h2 class="inline-flex mt-2">By Lary Laracore <img src="/images/lary-head.svg" alt="Head of Lary the mascot"></h2>
-
-    <p class="text-sm mt-14">
-        Another year. Another update. We're refreshing the popular Laravel series with new content.
-        I'm going to keep you guys up to speed with what's going on!
-    </p>
+    
+   
 
     <div class="flex items-center mt-8">
         <!--  Category -->
